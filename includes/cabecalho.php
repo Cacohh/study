9b3c17b0a9756d22120b675cf25ee6c2ad6f8195
Caio Souza <caiojorge2036@gmail.com>
2017-08-10 13:57:33 -0300

    Introduzindo PHP 1

---
+++ includes/cabecalho.php
@@ -0,0 +1,15 @@
+<header class="container">
+        <!-- Conteúdo do cabeçalho -->
+        <h1><img src="img/logo.png" alt="Mirror Fashion"></h1>
+
+        <p class="sacola">
+            Nenhum item na sacola de compras
+        </p>
+
+        <li><a href="#">Lista de Desejos</a></li>
+        <li><a href="#">Cartão Fidelidade</a></li>
+        <li><a href="sobre.php">Sobre</a></li>
+        <li><a href="#">Ajuda</a></li>
+        </ul>
+        </nav>
+    </header>
