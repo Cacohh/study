2bee8117d827b177ce25b8c5c89c4d5561054a35
Caio Souza <caiojorge2036@gmail.com>
2017-08-11 16:30:16 -0300

    Mobile-first and Progressive Enhanecement

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?php print $cabecalho_title; ?></title>
+<?php 
+ print @$cabecalho_css1;
+ print @$cabecalho_css2;
+ print @$cabecalho_css3;
+?>
+<meta name="viewport" content="width=device-width">
+</head>
+<body>
 <header class="container">
         <!-- Conteúdo do cabeçalho -->
         <h1><img src="img/logo.png" alt="Mirror Fashion"></h1>
@@ -5,7 +18,8 @@
         <p class="sacola">
             Nenhum item na sacola de compras
         </p>
-
+        <nav class="menu-opcoes">
+        <ul>
         <li><a href="#">Lista de Desejos</a></li>
         <li><a href="#">Cartão Fidelidade</a></li>
         <li><a href="sobre.php">Sobre</a></li>
